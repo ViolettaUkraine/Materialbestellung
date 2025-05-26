3990917f9b68b7806164f82ee5464038e1b89d3d
Violetta Martyn <641_000004672@tln.bfw-hamburg.de>
2025-05-26 15:38:23 +0200

    startseite

--- index.php
+++ index.php
@@ -1,3 +1,41 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+  <link rel="stylesheet" href="css/style1.css">
+  <link href="https://fonts.googleapis.com/css2?family=Caveat&display=swap" rel="stylesheet">
+  
+</head>
+<body>
+
+<div class="background-container">
+  <img src="css/bild33.avif" alt="Bild" class="background-image">
+</div>
+
+<div class="login-container">
+  <form method="post" class="login-form">
+    <h2 id="formTitle">Login</h2>
+    <input name="username" placeholder="Benutzername" required>
+    <input name="password" type="password" placeholder="Passwort" required>
+    <div id="roleSelect" style="display:none;">
+        <select name="role">
+        <option value="Besteller">Besteller</option>
+        <option value="Geschäftsstelle">Geschäftsstelle</option>
+        <option value="Admin">Admin</option>
+      </select>
+    </div>
+    <input type="hidden" name="action" id="formAction" value="login">
+    <button type="submit">Absenden</button>
+    
+    <p class="toggle-link" onclick="toggleForm()">Noch kein Konto? Jetzt registrieren</p>
+
+    <?php if (isset($error)) echo "<p class='error'>$error</p>"; ?>
+    <?php if (isset($success)) echo "<p class='success'>$success</p>"; ?>
+  </form>
+</div>
+
 <?php
 require_once 'includes/db.php';
 require_once 'includes/auth.php';
@@ -11,9 +49,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
-<form method="post">
-    <input name="username" placeholder="Benutzername" required>
-    <input name="password" type="password" placeholder="Passwort" required>
-    <button type="submit">Login</button>
-    <?php if (isset($error)) echo "<p>$error</p>"; ?>
-</form>
+<script src="java1.js"></script>
+
+</body>
+</html>
+
+
